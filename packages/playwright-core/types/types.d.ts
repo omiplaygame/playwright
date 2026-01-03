332e4349e81f74b62e8bdd72ff516b3b3ee13118
OmiPlay <xassomi@gmail.com>
2026-01-03 16:31:47 +0200

    Added publicIpHint to proxy LaunchOptions

--- packages/playwright-core/types/types.d.ts
+++ packages/playwright-core/types/types.d.ts
@@ -27,8 +27,6 @@ type ElementHandleWaitForSelectorOptionsNotHidden = ElementHandleWaitForSelector
   state?: 'visible'|'attached';
 };
 
-export type PublicIpHint = string | undefined;
-
 /**
  * Page provides methods to interact with a single tab in a [Browser](https://playwright.dev/docs/api/class-browser),
  * or an [extension background page](https://developer.chrome.com/extensions/background_pages) in Chromium. One
@@ -21858,10 +21856,6 @@ export interface LaunchOptions {
      * Optional password to use if HTTP proxy requires authentication.
      */
     password?: string;
-    /**
-	   * Public IP address from cache, received from outside of Camoufox.
-	  */
-	  publicIpHint?: PublicIpHint;
   };
 
   /**
