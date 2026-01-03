f2f2b02b28ac69778c47a6fdda8a64e55a8b34ae
OmiPlay <xassomi@gmail.com>
2026-01-03 16:35:22 +0200

    Added publicIpHint to proxy LaunchOptions

--- packages/playwright-core/types/types.d.ts
+++ packages/playwright-core/types/types.d.ts
@@ -27,6 +27,8 @@ type ElementHandleWaitForSelectorOptionsNotHidden = ElementHandleWaitForSelector
   state?: 'visible'|'attached';
 };
 
+export type PublicIpHint = string | undefined;
+
 /**
  * Page provides methods to interact with a single tab in a [Browser](https://playwright.dev/docs/api/class-browser),
  * or an [extension background page](https://developer.chrome.com/extensions/background_pages) in Chromium. One
@@ -21856,6 +21858,7 @@ export interface LaunchOptions {
      * Optional password to use if HTTP proxy requires authentication.
      */
     password?: string;
+    publicIpHint?: PublicIpHint;
   };
 
   /**
